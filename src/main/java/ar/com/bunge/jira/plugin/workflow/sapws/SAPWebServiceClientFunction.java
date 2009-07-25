a2509411f87bff1206c9d8dd5e7cfba9ed5d58a9
Mariano Capurro <marianocapurro@users.noreply.github.com>
2009-07-25 17:17:41 +0000

    Developed dummy post function that dumps workflow data and developed skeleton for SAP WS Client post funcion.

---
+++ src/main/java/ar/com/bunge/jira/plugin/workflow/sapws/SAPWebServiceClientFunction.java
@@ -0,0 +1,39 @@
+/*
+ * File name: SAPWebServiceClientFunction.java
+ * Creation date: Jul 25, 2009 12:06:40 PM
+ * Copyright Mindpool
+ */
+package ar.com.bunge.jira.plugin.workflow.sapws;
+
+import java.util.Map;
+
+import com.opensymphony.module.propertyset.PropertySet;
+import com.opensymphony.workflow.FunctionProvider;
+import com.opensymphony.workflow.WorkflowException;
+
+/**
+ *
+ * @author <a href="mcapurro@gmail.com">Mariano Capurro</a>
+ * @version 1.0
+ * @since SPM 1.0
+ *
+ */
+public class SAPWebServiceClientFunction implements FunctionProvider {
+
+	/**
+	 * 
+	 */
+	public SAPWebServiceClientFunction() {
+	}
+
+	/**
+	 * @param transientVars
+	 * @param args
+	 * @param ps
+	 * @throws WorkflowException
+	 * @see com.opensymphony.workflow.FunctionProvider#execute(java.util.Map, java.util.Map, com.opensymphony.module.propertyset.PropertySet)
+	 */
+	public void execute(Map transientVars, Map args, PropertySet ps) throws WorkflowException {
+	}
+
+}
